0bc1f8e396e0ae1dcb475a208d444bd6fb8a8377
mateusjose98 <josemateus.ufma@gmail.com>
2021-01-18 21:47:04 -0300

    Validação adicionada aos campos Cliente.class

--- osworks-api/src/main/java/com/algaworks/osworks/controller/ClienteController.java
+++ osworks-api/src/main/java/com/algaworks/osworks/controller/ClienteController.java
@@ -3,12 +3,16 @@ package com.algaworks.osworks.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -47,10 +51,34 @@ public class ClienteController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente adicionar(@RequestBody Cliente cliente ) {
+	public Cliente adicionar(@Valid @RequestBody Cliente cliente ) {
 		return clienteRepository.save(cliente); // parei aqui - min 57 da aula
 		
 		
+	}
+	
+	@PutMapping("/{clienteId}")
+	public ResponseEntity<Cliente> atualizar(@Valid @PathVariable Long clienteId,
+			@RequestBody Cliente cliente){
+		if (!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		cliente.setId(clienteId);
+		cliente = clienteRepository.save(cliente);
+		
+		return ResponseEntity.ok(cliente);
+		
+	}
+	
+	@DeleteMapping("/{clienteId}")
+	public ResponseEntity<Void> remover(@PathVariable Long clienteId){
+		if (!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		clienteRepository.deleteById(clienteId);
+		
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- osworks-api/src/main/java/com/algaworks/osworks/domain/model/Cliente.java
+++ osworks-api/src/main/java/com/algaworks/osworks/domain/model/Cliente.java
@@ -4,19 +4,28 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Cliente {
-	
+	//parei na parte 1h27 - video 2
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	
+	@NotBlank
+	@Size(max = 60)
 	private String nome;
+	
+	@NotBlank
+	@Email
+	@Size(max = 255)
 	private String email;
 	
-	
+	@NotBlank
+	@Size(max = 20)
 	private String telefone;
 	
 	
